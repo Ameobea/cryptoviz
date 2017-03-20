e14d7bfb8b4ca41022a63c7a92117d5533f0dcc2
Casey Primozic <me@ameo.link>
2017-03-20 17:25:49 -0500

    Initial Commit
     - Created boilerplate application using DvaJS
     - Added notes.txt with brainstorming and ideas

---
+++ src/routes/IndexPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'dva';
+
+function IndexPage() {
+  return (
+    <div />
+  );
+}
+
+IndexPage.propTypes = {
+};
+
+export default connect()(IndexPage);
