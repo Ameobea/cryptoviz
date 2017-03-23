5fb5b481a3296516e51e77e69b4aa80f8243c803
Casey Primozic <me@ameo.link>
2017-03-23 01:46:24 -0500

    Update .eslintrc and added dummy state
     - Updated .eslintrc to match that of react-orderbook
     - Set up some dummy state for the application's main component to trigger periodic updates for the orderbook vizualizations

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,9 +1,50 @@
+// @flow
+
 import React from 'react';
 
 import OrderbookVisualizer from 'react-orderbook';
 
-function IndexPage() {
-  return <OrderbookVisualizer />;
+class IndexPage extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.bookModificationExecutor = this.bookModificationExecutor.bind(this);
+    this.state = {
+      curTimestamp: 1000,
+      // ***DEV***
+      curPrice: 18.2342
+    };
+  }
+
+  componentShouldRender(nextProps) {
+    // never re-render
+    return false;
+  }
+
+  // ***DEV***
+  bookModificationExecutor(callback) {
+    // trigger a dummy update every second
+    setInterval(() => {
+      const update: {timestamp: number, price: number, newAmount: number, isBid: boolean} = {
+        timestamp: this.state.curTimestamp + 1,
+        price: 1.0,
+        newAmount: this.state.curPrice * 1.05,
+        isBid: true,
+      };
+
+      callback(update);
+      this.setState({curTimestamp: this.state.curTimestamp + 1, curPrice: this.state.curPrice * 1.05});
+    });
+  }
+
+  render() {
+    return (
+      <OrderbookVisualizer
+        bookModificationCallbackExecutor={this.bookModificationExecutor}
+        initialTimestamp={this.state.curTimestamp}
+      />
+    );
+  }
 }
 
 IndexPage.propTypes = {
